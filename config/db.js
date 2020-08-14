f77187d68bed0597abb029d3cb22d21331d2639d
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-08-14 12:56:53 -0700

    db connection

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// using async await, always wrap in try/catch block
+const connectDB = async () => {
+  try {
+    // mongoose connect returns a promise, so use await
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch(err) {
+    console.error(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
