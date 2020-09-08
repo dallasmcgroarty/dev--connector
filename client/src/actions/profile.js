9987bf09ba57c7e617cc174f8278af4c60cfe3cf
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-09-08 15:26:38 -0700

    add education and experience to profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  UPDATE_PROFILE
 } from './types';
 
 // get current users profile
@@ -42,6 +43,72 @@ export const createProfile = (formData, history, edit=false) => async dispatch =
     dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
     // (!edit) {} in case no redirect
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add experience
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type':'application/json'
+      }
+    }
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type':'application/json'
+      }
+    }
+
+    const res = await axios.put('/api/profile/education', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education added', 'success'));
+
     history.push('/dashboard');
   } catch (err) {
     const errors = err.response.data.errors;
